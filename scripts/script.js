9cbe381d1f54fd67bbd79c5e1a41b73c5420d74d
Nisan <nnisan858@gmail.com>
2022-06-23 20:10:34 +0300

    page 2 style + arrows

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,54 @@
 console.log("hello from script")
 
 let array = [];
+let currentSection = -1;
 
 function init() {
-    array = document.getElementsByClassName('slide')
-    const slidesEl = document.getElementsByClassName('box');
-    const sliddingContent = document.getElementById('slide-contant')
-    const fruitsLi = document.getElementById('fruits-li');
-    const explainBox = document.getElementById("explain-box");
+    array = document.getElementsByClassName('slide')    // get all html sections
+    const slidesEl = document.getElementsByClassName('box');    // get all fruits slides
+    // const sliddingContent = document.getElementById('slide-contant') //
+    const fruitsLi = document.getElementById('fruits-li'); // fruits li element
+    const explainBox = document.getElementById("explain-box");  // get explain box element
     const sliddingWindowEl = document.getElementById("slidding-window");
-    fruitsLi.addEventListener('click', () => {
-        explainBox.innerHTML = '';
+
+    const left = document.getElementById('left')
+    const right = document.getElementById('right')
+
+
+    fruitsLi.addEventListener('click', () => { // event tha open slidding window
+        explainBox.style.display = 'none';
         sliddingWindowEl.style.display = 'block'
-        explainBox.append(sliddingWindowEl);
     });
 
     for (let i = 0; i < slidesEl.length; i++) {
-        console.log(slidesEl[i]);
         slidesEl[i].addEventListener('click', (e) => {
-            for (let j = 0; j < slidesEl.length; j++) {
-                slidesEl[j].classList.remove('clicked');
-                array[j].style.display = 'none';
-            }
-            e.target.classList.add('clicked');
-            console.log(array[i])
-            array[i].style.display = "block";
+            currentSection = i;
+            updateSection();
         });
     }
 
+    left.addEventListener('click', () => {
+        currentSection = (currentSection + 1) % 4;
+        updateSection();
+    });
+    right.addEventListener('click', () => {
+        currentSection--;
+        if (currentSection < 0)
+            currentSection = slidesEl.length - 1;
+        updateSection();
+    })
+
+    function updateSection() {
+        for (let j = 0; j < slidesEl.length; j++) {
+            slidesEl[j].classList.remove('clicked');
+            array[j].style.display = 'none';
+        }
+        slidesEl[currentSection].classList.add('clicked');
+        array[currentSection].style.display = 'block';
+    }
 }
+
+
 
 
 window.addEventListener('load', () => {
