1e1fa4eb8e1bf5b2eb7addfcc7c5ef932292add2
Nisan <nnisan858@gmail.com>
2022-06-28 17:26:46 +0300

    chamges

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,34 +3,32 @@ console.log("hi from contact script")
 
 function validateForm(e) {
     e.preventDefault();
+
     const span = document.getElementById('error-msg');
     span.textContent = '';
     let errorMessage = '';
 
     const mailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const phonePattern = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
+
     const fname = document.forms["myForm"]["fname"].value;
     const lname = document.forms["myForm"]["lname"].value;
     const fhone = document.forms["myForm"]["fhone"].value;
     const mail = document.forms["myForm"]["mail"].value;
     const subject = document.forms["myForm"]["subject"].value;
 
-    if (!fname.length) {
+    if (fname === '') {
         errorMessage = 'first name is required';
         span.textContent = errorMessage;
         return;
     }
-    if (!lname.length) {
+    if (lname === '') {
         errorMessage = 'last name is required';
         span.textContent = errorMessage;
         return;
     }
-    if (!lname.length) {
-        errorMessage = 'last name is required';
-        span.textContent = errorMessage;
-        return;
-    }
-    if (!fhone.length) {
+    if (fhone === '') {
         errorMessage = 'phone number is required';
         span.textContent = errorMessage;
         return;
@@ -50,6 +48,5 @@ function validateForm(e) {
         span.textContent = errorMessage;
         return;
     }
-
 }
 
